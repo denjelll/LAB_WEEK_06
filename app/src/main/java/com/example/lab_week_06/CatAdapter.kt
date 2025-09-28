51e321854b2f5025a1d080379a8cd80a43a38f6c
denjelll <165755569+denjelll@users.noreply.github.com>
2025-09-28 16:24:44 +0700

    Commit No.2 - Clickable RecyclerView

--- app/src/main/java/com/example/lab_week_06/CatAdapter.kt
+++ app/src/main/java/com/example/lab_week_06/CatAdapter.kt
@@ -5,30 +5,39 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lab_week_06.model.CatModel
 
-class CatAdapter(private val layoutInflater: LayoutInflater, private val
-imageLoader: ImageLoader) : RecyclerView.Adapter<CatViewHolder>() {
-    //Mutable list for storing all the list data
+class CatAdapter(
+    private val layoutInflater: LayoutInflater,
+    private val imageLoader: ImageLoader,
+    private val onClickListener: OnClickListener
+) : RecyclerView.Adapter<CatViewHolder>() {
+
+    // Mutable list for storing all the list data
     private val cats = mutableListOf<CatModel>()
-    //A function to set the mutable list
+
+    // A function to set the mutable list
     fun setData(newCats: List<CatModel>) {
         cats.clear()
         cats.addAll(newCats)
-//This is used to tell the adapter that there's a data change in the mutable list
-                notifyDataSetChanged()
+        // Tell the adapter that the data has changed
+        notifyDataSetChanged()
     }
-    //A view holder is used to bind the data to the layout views
-//onCreateViewHolder is instantiating the view holder it self
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
-            CatViewHolder {
+
+    // onCreateViewHolder is instantiating the view holder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatViewHolder {
         val view = layoutInflater.inflate(R.layout.item_list, parent, false)
-        return CatViewHolder(view, imageLoader)
+        return CatViewHolder(view, imageLoader, onClickListener)
     }
-    //This is used to get the amount of data/item in the list
+
+    // Get the amount of data/item in the list
     override fun getItemCount() = cats.size
-    //This is used to bind each data to each layout views
+
+    // Bind each data to each layout views
     override fun onBindViewHolder(holder: CatViewHolder, position: Int) {
-//The holder parameter stores our previously created ViewHolder
-//The holder.bindData function is declared in the CatViewHolder
         holder.bindData(cats[position])
+    }
+
+    // Custom listener (not android.view.View.OnClickListener)
+    interface OnClickListener {
+        fun onItemClick(cat: CatModel)
     }
 }
